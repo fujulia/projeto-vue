7c51a5cbbee1e5553f81744ba50845fa23a59450
fujulia <juliafu713@gmail.com>
2024-02-19 16:55:24 -0300

    alteraçoes_iniciais

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,21 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+const contador = ref(0)
+
+/*let nome= "Eduardo"
+const pessoa= {
+  nome: "Eduardo",
+  cidade: "São Paulo"
+}
+pessoa.nome="nova.pessoa"*/
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <button @click="contador++">+</button>
+  <button @click="contador--">-</button>
+ <p>Valor do contador: {{ contador }}</p>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
