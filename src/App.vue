ba6f28f49f58309711ff5c2fe8f177d54b273822
fujulia <juliafu713@gmail.com>
2024-02-23 12:07:34 -0300

    add button zerar, decrementar positivos

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <script setup>
-import { ref } from 'vue'
-const contador = ref(0)
+import { ref } from 'vue';
+const contador = ref(0);
+
+function incrementarContador(){
+  contador.value++
+}
+
+function decrementarContador(){
+  if(contador.value>0){
+    contador.value--;
+  }
+}
+
+function zerarContador(){
+  contador.value = 0;
+}
 
 /*let nome= "Eduardo"
 const pessoa= {
@@ -8,14 +22,22 @@ const pessoa= {
   cidade: "São Paulo"
 }
 pessoa.nome="nova.pessoa"*/
+
 </script>
 
 <template>
-  <button @click="contador++">+</button>
-  <button @click="contador--">-</button>
+  <button @click="incrementarContador()">+</button>
+  <button @click="decrementarContador()">-</button>
+  <button @click="zerarContador()">Zerar</button>
+
  <p>Valor do contador: {{ contador }}</p>
 </template>
 
 <style scoped>
+  
+  button {
+  font-weight: bold;
+
+}
 
 </style>
